0f6df7f6c23bfe5850cb68869ff79054df3c610d
Francisco <montejof1998@gmail.com>
2023-04-17 19:38:19 -0700

    working on the styles for the components

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Pictures } from "./Pictures";
+import "../styles/global.scss";
+
+const App = () => {
+  return <Pictures></Pictures>;
+};
+
+export default App;
---
+++ src/components/Pictures.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/global.scss";
+import "../styles/Pictures.scss";
+import { Picture } from "./Picture";
+
+const Pictures = () => {
+  return (
+    <main className="mainContainer">
+      <div className="search">
+        <input className="input" type="text" />
+        <button className="button">SEARCH</button>
+      </div>
+      <section className="picturesContainer">
+        <Picture></Picture>
+      </section>
+    </main>
+  );
+};
+
+export { Pictures };
